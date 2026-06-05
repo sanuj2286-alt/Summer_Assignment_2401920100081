326ec71a0f3209a2a886be277a1655ff8e596ff0
Anuj Saini <sanuj@2286gmail.com>
2026-06-05 20:00:35 +0530

    Week_1 Day_5

---
+++ Week_1/Day_5/1.Valid_palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+
+        for (char ch : s) {
+            if (isalnum(ch)) {
+                temp += tolower(ch);
+            }
+        }
+
+        string rev = temp;
+        reverse(rev.begin(), rev.end());
+
+        return temp == rev;
+    }
+};
