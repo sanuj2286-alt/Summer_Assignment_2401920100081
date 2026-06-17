56241f26ce48e684c2d0753a03596ae9c887781f
Anuj Saini <sanuj2286@gmail.com>
2026-06-17 21:53:21 +0530

    Week_3 Day_2

---
+++ Week_3/Day_2/3.Palindrome_linkedList.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow->next;
+
+        while(curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Compare both halves
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while(second) {
+            if(first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
